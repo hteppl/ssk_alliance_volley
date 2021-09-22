594c7a6ae84dab5ee991e846f990d8a360048f6d
hteppl <hteppl.dev@gmail.com>
2021-09-22 19:06:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
+import datetime
 import random
+
 import vk_api
 from time import sleep
 
-token = ''
+token = ''  # vkhost.github.io
 name = 'ФИО'
 
-owner = -202633307
 str_1 = 'Запись на ИГРОВУЮ ТРЕНИРОВКУ 23'
 str_2 = 'Уровень «любой»'
 
 vkq = vk_api.VkApi(token=token)
+owner = -202633307
+count = 3
 
 print(name + " hosted!")
 
 while True:
-    sleep(random.randint(35, 65))
-    wall = vkq.method('wall.get', {'owner_id': owner, 'count': 2})
-    print('Wall getted!')
-    post = wall['items'][0]
-    text = str(post['text'])
+    sleep(random.randint(10, 15))
+    wall = vkq.method('wall.get', {'owner_id': owner, 'count': count})
+    print(str(datetime.datetime.now()) + ' Wall getted!')
 
-    if str_1 in text and str_2 in text:
-        sleep(2)
-        vkq.method('messages.send',
-                   {'peer_id': owner, 'random_id': random.randint(111111, 999999), 'message': 'Секция ' + name})
-        print('Entry catched!')
-        exit()
+    for i in range(count):
+        post = wall['items'][i]
+        text = str(post['text'])
+
+        if str_1 in text and str_2 in text:
+            sleep(1)
+            vkq.method('messages.send',
+                       {'peer_id': owner, 'random_id': random.randint(111111, 999999), 'message': 'Секция ' + name})
+            print(str(datetime.datetime.now()) + 'Entry catched!')
+            exit()
